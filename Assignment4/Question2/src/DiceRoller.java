847043f5459721ee57a00e7a59313073e4640c7b
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2024-04-04 17:47:37 -0600

    Initial check in

---
+++ Assignment4/Question2/src/DiceRoller.java
@@ -0,0 +1,23 @@
+/* Author: Caitlin Coulombe, T00756521
+ * Date: April 4, 2024
+ * Course: COMP 1131
+ * Assignment 4 Question 1: The Die class presented in Listing 5.2 of the textbook models a standard cubic die with faces 1 through 6. 
+ * It is also possible to have regular dice with 4 sides (tetrahedron), 8 sides (octahedron), 12 sides (dodecahedron), and 20 sides 
+ * (icosahedron).
+ * 
+ * For your design, create a UML Class diagram similar to Figure 5.5 on page 180 of the textbook. Note that you need to include the 
+ * constructors in the methods section.
+ * 
+ *      Testing: Write a test driver that exercises this class as follows: 
+ *          * Instantiates a 6-sided die using the default constructor, then outputs the result of 12 rolls.
+ *          * Create one of each of the five regular dice using the second constructor. For each, output the results from number of 
+ *          rolls that is double the number of faces created
+ *          * Attempt to create a die with an illegal number of faces then output the results of 10 rolls.
+ * 
+ */
+
+public class DiceRoller {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ Assignment4/Question2/src/Die.java
@@ -0,0 +1,20 @@
+/* Author: Caitlin Coulombe, T00756521
+ * Date: April 4, 2024
+ * Course: COMP 1131
+ * Assignment 4 Question 1: Modify the Die class so that it can be used to simulate any of the five possible regular dice. The class 
+ * requires the following methods and/or modifications:
+ *      * A constructor with no parameters that creates a default 6-sided die
+ *      * A consturctor with one parameter that indicates how many faces the die is to have. Check that it is one of the permitted 
+ *      values, otehrwise default to a 6-sided die.
+ *      * The roll() method given needs to be modified to work within the number of faces established by the constructors.
+ *      * The setFaceValue() method needs to have logic added to ensure the new incoming face value is within the allowable limits 
+ *      for the number of faces.
+ *      * A second get_ method that retrieves the textual name of the current face value, such as One, Two, Thre, &c. Note that a 
+ *      single method i expected that will work with any of the five regular dice.
+ *      * The toString() method should return both the numeric value and textual name for the current face value in a form like "1 : 
+ *      One".
+ */
+
+public class Die {
+
+}
