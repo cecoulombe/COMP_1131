2d41b9c243348020f2596fe640a7f2c8736c65bf
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2024-04-20 22:57:26 -0600

    Debugging and testing q3

--- Assignment5/Question3/src/FibonacciDriver.java
+++ Assignment5/Question3/src/FibonacciDriver.java
@@ -9,8 +9,58 @@
  * For the main driver, produce at least five sequences, including 1 and 2 as boundary tests.
  */
 
+ import java.util.Scanner;
+
 public class FibonacciDriver {
     public static void main(String[] args) throws Exception {
+        // take a value in from the user for N 
+        // call the static sequence and print the results
+
+        // Variables
+        int inputValue;
+
+        final int FIRST_NUM = 1;    // I have chosen 1 as my first number; however, the Fibonacci sequence can be started at 0 or the logic can be applied to any starting value
+        final int SECOND_NUM = 1;
+
+        Scanner scan = new Scanner(System.in);
+
+        // prompt the user for the value they want to know
+        System.out.print("Please enter a non-zero, positive integer value of the position in the Fibonacci sequence you want to know: ");
+
+        inputValue = scan.nextInt();
+        System.out.println();
+
+        while(inputValue <= 0)
+        {
+            System.out.print("The value must be a whole number that is equal to or greater than 1: ");
+
+            inputValue = scan.nextInt();
+            System.out.println();
+        }
+
+        // call the static method to get the value from the sequence
+        int value = FibonacciSequence.fibSum(inputValue, FIRST_NUM, SECOND_NUM);
         
+        // determining the proper suffix for N 
+        String valueSuffix = "";
+        if(inputValue == 1)
+        {
+            valueSuffix = "st";
+        }
+        else if(inputValue == 2)
+        {
+            valueSuffix = "nd";
+        }
+        else if(inputValue == 3)
+        {
+            valueSuffix = "rd";
+        }
+        else
+        {
+            valueSuffix = "th";
+        }
+
+        // tell the user the Nth value
+        System.out.println("The " + inputValue + valueSuffix + " value using the Fibonacci sequence is " + value);
     }
 }
--- Assignment5/Question3/src/FibonacciSequence.java
+++ Assignment5/Question3/src/FibonacciSequence.java
@@ -1,3 +1,31 @@
+/* Author: Caitlin Coulombe, T00756521
+ * Date: April 20, 2024
+ * Course: COMP 1131
+ * Assignment 5 Question 5: See FibonacciDriver for assignment details.
+ */
+
+// The Fibonacci sequence is a sequency of numbers where one number is the sum of the two numbers prior to it in the sequence.
+// Recursion will be used to determine the Nth number in the sequence, where N is determined by the driver.
 public class FibonacciSequence {
+    // create a recursive algorithm for the fibonnaci sequence 
+    // n_5 = (n_4) + (n_3)
+    // n_4 = (n_3) + (n_1)
+    // n_3 = (n_2) + (n_1)
+    
+    //-------------------------------------------------------------------------------------------------------------------------
+    //fibSum: recursively determines the target value per the logic in the algorithm above.
+    //-------------------------------------------------------------------------------------------------------------------------
+    public static int fibSum(int targetValue, int num1, int num2)
+    {
+        //pass add num 1 and num 2, then pass num 2 as num1 and sum as num2 until the counter hits zero
+        if(targetValue == 1)
+        {
+            return num1;
+        }
+        else
+        {
+            return fibSum(targetValue - 1, num2, (num1 + num2));
+        }
+    }
 
 }
